7d29db215fb9a12bf054a8b818886ff2497b9cf7
Joan <bd7672a@gmail.com>
2024-05-10 16:27:06 -0600

    Excluded AMD DVR Overlay

--- core/topmost_window.py
+++ core/topmost_window.py
@@ -17,7 +17,7 @@ def enumerate_windows():
 def should_exclude_process(name):
     excluded_processes = ['dwm.exe', 'nvcontainer.exe', 'nvidia broadcast ui.exe', 'system', 'python.exe', 'steam.exe',
                           'TextInputHost.exe', 'tk', 'pycharm64.exe', 'nvidia broadcast.exe', 'widgets.exe',
-                          'CTkToplevel', 'Windows Input Experience', 'widgets.exe', 'translucenttb.exe', 
+                          'CTkToplevel', 'Windows Input Experience', 'widgets.exe', 'translucenttb.exe', 'amdow.exe',
                           'securityhealthsystray.exe', 'Ctk', 'Ctk.exe', 'tk', 'tk.exe', 'Code', 'Code.exe', 'NVIDIA Share.exe',
                           'NVIDIA Web Helper.exe', 'nvsphelper64.exe', 'NVIDIA GeForce Experience.exe',
                           'nvcontainer.exe', 'NVDisplay.Container.exe']
